737b863cea0504b408dabf7f6a9c25d18baefb2a
antonborod1n <antonborodin86@gmail.com>
2022-04-22 08:10:18 +0300

    added main prototype

--- js/homework8.js
+++ js/homework8.js
@@ -1,63 +1,27 @@
 function HashStorageFunc() {
 
-    let obj = {};
+    let _obj = {};
 
-    this.addValue = function(key,value) {
-        obj[key] = value;   
+    this.addValue = function () {
+        var name = prompt("введите название напитка");
+        var isAlc = prompt("он алкогольный?")
+        var structure = prompt("способ приготовления");
+        _obj[name] = name;
+        _obj[isAlc] = isAlc;
+        _obj[structure] = structure;
+
     };
-} 
+    this.getValue = function () {
+
+    };
+    this.deleteValue = function () {
+
+    };
+    this.getKeys = function () {
+
+    };
+    
+}
 
 var drinkStorage = new HashStorageFunc();
 
-var name = prompt("введите название напитка");
-var isAlc = prompt("он алкогольный?")
-var structure= prompt("способ приготовления");
-var result = isAlc + " " + structure;
-
-drinkStorage.addValue(name, result);
-
-console.log(drinkStorage);
-
-
-
-
-
-
-
-
-/* let obj = {};
-
-function addValue() {
-    var drink = prompt('Введите название напитка');
-    var d1 = confirm('Он алкогольный?');
-    var d2 = prompt('Введите рецепт его приготовления');
-    
-    obj.nam = d;
-    obj.alc = d1;
-    obj.rec = d2;
-    return obj;
-}
-
-console.log(obj);
-
-function getValue() {
-    var d = prompt('Введите название напитка');
-    if(d === arr) {
-        alert(d);
-    } else {
-        alert('Такой напиток отсутствует');
-    }
-}
-
-function deleteValue() {
-    var drink = prompt('Введите название напитка');
-    deleteDrink(drink);
-}
-
-function deleteDrink(drink) {
-    delete obj[nam];
-}
-
-function getKeys() {
-    alert('Все напитки');
-} */
--- js/main.js
+++ js/main.js
@@ -656,3 +656,63 @@ function rev(arr) {
 }
 
 rev(arr); */
+
+
+
+/* Разработка приложения “Книги”.
+1. Объявить класс под названием “Book”
+2. Класс “Book”. У каждой книги(созданного объекта от класса Book) есть “название”, “автор”, “количество экземпляров”, “количество страниц”.
+ Есть метод который выводит количество страниц книги.
+ Есть метод который считает количество глав(1 глава равна 10 страницам).
+
+
+3. Класс “Journal”. У каждого журнала(созданного объекта от класса Journal)есть поля: "название", “год выпуска”, "месяц выпуска," номер выпуска”, “количество страниц”.
+ Есть метод который выводит данные о количестве страниц.
+ Есть метод, который выводит такие данные как: "номер выпуска", "год выпуска", "месяц выпуска".
+ */
+
+
+ function Book(title, countPages, author, countCopies ){
+  this.title = title;
+  this.author = author;
+  this.countCopies = countCopies;
+  this.countPages = countPages;
+}
+
+Book.prototype.getCountPages = function(){
+  return this.countPages;
+};
+
+Book.prototype.getCountChapters = function(){
+  return Math.round(this.countPages / 10);
+};
+
+var book = new Book('Titl', 'Max', 199, 27);
+var book1 = new Book('Titl2', 'Max2', 1993, 273);
+console.log(book);
+console.log(book1);
+console.log(book.getCountPages());
+console.log(book.getCountChapters());
+
+function Journal(title, countPages, yearProd, monthProd, numbProd){
+ Book.call(this, title, countPages);
+ this.yearProd = yearProd;
+ this.monthProd = monthProd;
+ this.numbProd = numbProd;
+}
+
+// Прототипное наследование
+Journal.prototype = Object.create(Book.prototype);
+Journal.prototype.constructor=Journal;
+
+
+Journal.prototype.getData = function(){
+ return this.yearProd + ' ' + this.monthProd + ' ' + this.numbProd;
+};
+
+var journal = new Journal('NewT', 199, 2022, 04, 10);
+console.log(journal);
+var journal2 = new Journal('NewT2', 1992, 2022, 04, 10);
+console.log(journal2);
+console.log(journal.getCountPages());
+onsole.log(journal.getData())
