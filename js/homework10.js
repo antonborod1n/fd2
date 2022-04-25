2dc756de4d1cec6aad43bd5b6dc472ac28616b13
antonborod1n <antonborodin86@gmail.com>
2022-04-25 08:11:27 +0300

    added homework10

---
+++ js/homework10.js
@@ -0,0 +1,76 @@
+//N.10 Домашнее задание LOGIC_TASKS
+
+/* 1.  Напишите функцию сортировки, которая сортирует числа не по порядку номеров, а по их длине! Это означает сортировку чисел с наименьшим количеством цифр 
+в первую очередь до чисел с наибольшим количеством цифр.
+ 
+numbSort([2, 37, 1, 2, 739, 2]) ➞ [1, 2, 2, 2, 37, 739]
+numbSort([777, 182, 11, 881, 27]) ➞ [11, 27, 777, 182, 881]
+numbSort([5, 4, 3, 2, 1, 18, 6, 1, 3]) ➞ [5, 4, 3, 2, 1, 6, 1, 3, 18]
+ */
+
+/*function numbSort(arr) {
+    arr.sort(function (a, b) {
+        if (a.length > b.length) {
+            return 1;
+        }
+        if (a.length < b.length) {
+            return -1;
+        }
+    });
+    return arr; 
+}
+
+ console.log(numbSort([2, 37, 1, 2, 739, 2]));
+console.log(numbSort([777, 182, 11, 881, 27]));
+console.log(numbSort([5, 4, 3, 2, 1, 18, 6, 1, 3])); */
+
+
+/* 2. Напишите функцию, которая сортирует слова по заданной строке.
+В строке могут быть лишние буквы (см. Примеры #2 и #3).
+В каждом массиве будут уникальные начальные буквы.
+
+sortStr(["orange ", "mandarin", " pear", "coconut "], "cmop")➞ [" coconut", " mandarin", " orange", " pear"]
+sortStr(["middle", "junior", "senior"], "stnmj")➞ ["senior", " middle", " junior"]
+sortStr(["bike", "car", "motorcycle"], "camrb")➞ ["car", "motorcycle", "bike"]
+ */
+
+/* function sortStr(arr) {
+    var a = 'cmop';
+    var b = 'stnmj';
+    var c = 'camrb';
+
+    arr.filter(function(item){
+       return arr;
+
+    });
+}
+
+sortStr(["orange ", "mandarin", " pear", "coconut "], "cmop");
+sortStr(["middle", "junior", "senior"], "stnmj");
+sortStr(["bike", "car", "motorcycle"], "camrb"); */
+
+
+/* 
+3. Есть строка, переверните все слова нечетной длины. Слова четной длины не изменяются.
+revOddWord("Egg") ➞ "ggE"revOddWord ("It is false") ➞ "It is eslaf"revOddWord ("Dear Santa, give me a new iPhone”)➞ "Dear atnaS, give me a wen iPhone. " */
+
+
+
+function revOddWord(str) {
+    var arr = str.split(' ');
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length % 2 === 1) {
+            arr[i] = arr[i].split('').reverse().join('');
+        }
+    }
+    var res = arr.join(' ');
+    return res;
+}
+
+console.log(revOddWord("Egg"));
+console.log(revOddWord("It is false"));
+console.log(revOddWord('Dear Santa, give me a new iPhone'));
+
+
+
+
--- js/homework8.js
+++ js/homework8.js
@@ -1,27 +1,50 @@
-function HashStorageFunc() {
+/* function HashStorageFunc() {
+    let obj = {};
 
-    let _obj = {};
-
-    this.addValue = function () {
-        var name = prompt("введите название напитка");
-        var isAlc = prompt("он алкогольный?")
-        var structure = prompt("способ приготовления");
-        _obj[name] = name;
-        _obj[isAlc] = isAlc;
-        _obj[structure] = structure;
-
+    this.addValue = function (key, value) {
+        obj[key] = value;
     };
-    this.getValue = function () {
-
+    this.getValue = function (key) {
+        key = prompt('Название коктейля: ');
+        if (key in obj) {
+            alert('Напиток: ' + key + ' ' + 'Алкогольный: ' + obj.value);
+        }
+        else {
+            alert('Такого коктейля в списке нет');
+        }
     };
-    this.deleteValue = function () {
-
+    this.deleteValue = function (key) {
+        key = prompt('Название коктейля: ');
+        if (key in obj) {
+            delete obj[key];
+            alert('Удален');
+            console.log('true');
+        }
+        else {
+            alert('Такого коктейля в списке нет');
+            console.log('false');
+        } 
     };
-    this.getKeys = function () {
-
+    this.getKeys = function (key) {
+        for (key in obj) {
+            alert(key);
+        }
     };
-    
+
+    console.log(obj);
 }
 
 var drinkStorage = new HashStorageFunc();
+
+var name = prompt("введите название напитка");
+var isAlc = prompt("он алкогольный?");
+var structure = prompt("способ приготовления");
+var result = isAlc + " " + structure;
+
+drinkStorage.addValue(name, result); */
+
+
+
+
+
 
